0dcd73e3005c7b244fa562fb6e0053fec5b7dec6
Maurizio <maufel@gmail.com>
2019-12-15 21:45:45 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,67 +1,3 @@
-class Header extends React.Component{
-    render(){
-        return <header>Header</header>;
-    }
-}
-
-class Footer extends React.Component{
-    render(){
-        return <footer>Footer</footer>;
-    }
-}
-
-class Main extends React.Component{
-
-    constructor(){
-        super()
-        this.state = {
-            counter: 0
-        };
-    }
-
-    onIncrement() {
-        console.log('Incrementa');
-        this.setState({
-            counter: this.state.counter + 1
-        });
-    }
-
-    onDecrement() {
-        console.log('Decrementa');
-        this.setState({
-            counter: this.state.counter - 1
-        });
-    }
-
-    render(){
-        const {counter} = this.state;
-
-        return <main>
-            <button onClick={(e) => this.onIncrement()}>Incrementa</button>
-            <button onClick={(e) => this.onDecrement()}>Decrementa</button>
-            <div> Contatore =  {counter}</div>
-        </main>;
-    }
-}
-
-// Come passare le informazion ad un componente
-class AppHeader extends React.Component {
-    render() {
-        
-        // Variabile di stato visibile solo nella console
-        console.log(this.props);
-        
-        const { titolo} = this.props; // ES 6
-        
-        return (
-            
-            <div>
-                <h1>{titolo}</h1>
-            </div>
-        );
-    }
-}
-
 class ButtonDecrement extends React.Component {
 
     decrement() {
@@ -124,4 +60,3 @@ class App extends React.Component {
 }
 
 ReactDOM.render(<App titolo="Titolo App" welcome="Benvenuto nella nostra app"  />,document.getElementById("root")); 
-/* ReactDOM.render(<Titolo titolo="Titolo App" welcome="Benvenuto nella nostra app" />,document.getElementById("root")); */
